b0977f941b3c4d1f46db41a1f9e994799b223c79
Eric Abides <ericxdu23@gmail.com>
2026-09-13 23:06:20 -0700

    Add header.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 /* Child components. */
 import Footer from './components/Footer'
 import Game from './components/Game'
+import Header from './components/Header'
 
 export default function App () {
   return (
     <>
+      <Header />
       <Game />
       <Footer />
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header () {
+  return (
+    <header>
+      <h1>Musket, Neptune, Knapsack</h1>
+      <p>A <em>Noughts and Crosses</em> game.</p>
+    </header>
+  )
+}
+
+export default Header
